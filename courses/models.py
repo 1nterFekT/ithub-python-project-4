7bee4d4f3ac2b9218395e12e2d39bfb26939745e
1nterFekT <lavamishals@gmail.com>
2026-05-30 14:54:18 +0300

    courses/models and staff/models

---
+++ courses/models.py
@@ -0,0 +1,105 @@
+from django.db import models
+from students.models import Group
+
+class Discipline(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name='Название дисциплины'
+    )
+
+    duration = models.PositiveIntegerField(
+        verbose_name='Длительность'
+    )
+
+    curriculum = models.FileField(
+        upload_to='curriculums/',
+        null=True,
+        blank=True,
+        verbose_name='Учебный план'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата обновления'
+    )
+
+    class Meta:
+        verbose_name = 'Дисциплина'
+        verbose_name_plural = 'Дисциплины'
+        ordering = ['-updated_at', '-created_at']
+        indexes = [
+            models.Index(fields=['title']),
+            models.Index(fields=['updated_at']),
+        ]
+
+    def __str__(self):
+        return self.title
+    
+
+class Course(models.Model):
+    code = models.CharField(
+        max_length=15,
+        verbose_name='Код курса'
+    )
+
+    about = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True,
+        verbose_name='Описание'
+    )
+
+    course_start = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата начала'
+    )
+
+    course_end = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата завершения'
+    )
+
+    discipline = models.ForeignKey(
+        'courses.Discipline',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='courses',
+        verbose_name='Дисциплина'
+    )
+
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='courses',
+        verbose_name='Учебная группа'
+    )
+
+    teacher = models.ForeignKey(
+        'staff.Teacher',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='courses',
+        verbose_name='Преподаватель'
+    )
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ['discipline__title']
+        indexes = [
+            models.Index(fields=['code']),
+            models.Index(fields=['course_start']),
+            models.Index(fields=['course_end']),
+        ]
+
+    def __str__(self):
+        return self.code
