00b689a801db90f6d2dcec0c75fa62e6c2e60461
1nterFekT <lavamishals@gmail.com>
2026-06-04 18:38:58 +0300

    Students and teachers admin table

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from staff.models import Teacher
 
-# Register your models here.
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "initials", "account")
+    search_fields = ("last_name",)
+
+    def initials(self, obj):
+        return f"{obj.first_name[0]}. {obj.middle_name[0] + '.' if obj.middle_name else ''}"
+    
+    initials.short_description = "Инициалы"
--- students/admin.py
+++ students/admin.py
@@ -3,9 +3,20 @@ from students.models import Student, Group
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "account", "group")
-    search_fields = ("first_name", "last_name", "account__username")
+    list_display = (
+        "last_name",
+        "initials",
+        "group",
+        "account"
+    )
+
+    search_fields = ("last_name",)
     list_filter = ("group",)
+
+    def initials(self, obj):
+        return f"{obj.first_name[0]}. {obj.middle_name[0] + '.' if obj.middle_name else ''}"
+    
+    initials.short_description = "Инициалы"
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
