ae7dd112670e80be252478409eee81e1c461a702
1nterFekT <lavamishals@gmail.com>
2026-06-04 18:52:53 +0300

    Courses admin table

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from courses.models import Discipline
+from courses.models import Discipline, Course
 
 @admin.register(Discipline)
 class DisciplineAdmin(admin.ModelAdmin):
@@ -12,3 +12,41 @@ class DisciplineAdmin(admin.ModelAdmin):
 
     list_filter = ("title", "duration")
     search_fields = ("title",)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        "code",
+        "discipline_title",
+        "discipline_duration",
+        "course_start",
+        "course_end",
+        "teacher",
+        "group"
+    )
+
+    list_filter = (
+        "teacher",
+        "group",
+        "course_start",
+        "course_end"
+    )
+
+    search_fields = (
+        "code",
+        "discipline__title"
+    )
+
+    def discipline_title(self, obj):
+        return obj.discipline.title
+    
+    def discipline_duration(self, obj):
+        return obj.discipline.duration
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        if request.user.groups.filter(name="teachers").exists():
+            return qs.filter(teacher__acount=request.user)
+        
+        return qs
