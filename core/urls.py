967c994ecef58592fdcc5cebe37ffeb82ee5bf02
1nterFekT <lavamishals@gmail.com>
2026-06-04 17:54:45 +0300

    student functionallity

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from accounts import views as auth_views
 from students import views as student_views
 
@@ -11,4 +11,6 @@ urlpatterns = [
 
     path("courses/", student_views.courses_list, name="courses"),
     path("courses/<int:course_id>/", student_views.course_detail, name="course_detail"),
+
+    path("topics/<int:topic_id>/", student_views.topic_detail, name="topic_detail"),
 ]
--- students/views.py
+++ students/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from django.db.models import Sum
 
-from courses.models import Course
+from courses.models import Course, Topic
 from assignments.models import Submission, Assignment
 
 def get_student_or_none(user):
@@ -68,13 +70,64 @@ def course_detail(request, course_id):
         assignment = getattr(topic, "assignment", None)
         submission = submission_map.get(topic.id)
 
+        status = "not_submitted"
+
+        if submission:
+            if submission.score and submission.score > 0:
+                status = "graded"
+            else:
+                status = "pending"
+        
         topics_data.append({
             "topic": topic,
             "assignment": assignment,
-            "submission": submission
+            "submission": submission,
+            "status": status
         })
 
     return render(request, "students/course_detail.html", {
         "course": course,
         "topics": topics_data
+    })
+
+@login_required(login_url="login")
+@require_http_methods(["GET", "POST"])
+def topic_detail(request, topic_id):
+    student = get_student_or_none(request.user)
+
+    if not student:
+        return HttpResponseForbidden("Только студенты имеют доступ к этой странице")
+
+    topic = get_object_or_404(
+        Topic,
+        id=topic_id
+    )
+
+    assignment = getattr(topic, "assignment", None)
+
+    submission = None
+    if assignment:
+        submission = Submission.objects.filter(
+            student=student,
+            assignment=assignment
+        ).first()
+
+    if request.method == "POST" and assignment and not submission:
+        answer = request.POST.get("answer")
+
+        if answer:
+            Submission.objects.create(
+                student=student,
+                assignment=assignment,
+                answer=answer,
+                score=0
+            )
+
+            messages.success(request, "Ответ отправлен на проверку")
+            return redirect("topic_detail", topic_id=topic.id)
+
+    return render(request, "students/topic_detail.html", {
+        "topic": topic,
+        "assignment": assignment,
+        "submission": submission
     })
